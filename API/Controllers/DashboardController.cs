5525dc5541246e8e3b383086c62d53bba10bc600
redwoodcutter <oguzkumcular@gmail.com>
2021-05-31 22:29:36 +0300

    Delete Handler Added

--- API/Controllers/DashboardController.cs
+++ API/Controllers/DashboardController.cs
@@ -32,6 +32,11 @@ namespace API.Controllers
             dashboard.Id = id;
             return Ok(await Mediator.Send(new Edit.Command{Dashboard = dashboard}));
         }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+        }
 
     }
 }
