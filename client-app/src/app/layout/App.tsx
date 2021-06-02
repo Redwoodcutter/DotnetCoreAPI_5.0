a675e959560521d2a80c47f4336d89b0af0b9d10
redwoodcutter <oguzkumcular@gmail.com>
2021-06-02 23:39:54 +0300

    Typescript activity Interface

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from 'react';
 import './styles.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import {Activity} from '../models/activity'
 
 
 
 function App() {
-  const [dashboard, setDashboard] = useState ([]);
+  const [dashboard, setDashboard] = useState<Activity[]> ([]);
 
   useEffect(() => {
-    axios.get ('http://localhost:5000/api/dashboard').then(response => {
+    axios.get<Activity[]> ('http://localhost:5000/api/dashboard').then(response => {
       console.log(response);
       setDashboard(response.data);
     })
@@ -19,7 +20,7 @@ function App() {
     <div>
       <Header as='h2' icon='users' content='BestDb'/>
         <List>
-          {dashboard.map((dashboard: any) => (
+          {dashboard.map(dashboard => (
             <List.Item key={dashboard.id}>
               {dashboard.title}
             </List.Item>
