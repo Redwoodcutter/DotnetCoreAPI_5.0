6c2cb5e4af4e593a443d61cc0117749b1eb5f969
redwoodcutter <oguzkumcular@gmail.com>
2021-06-15 23:23:20 +0300

    Styling the activities

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,45 +1,17 @@
 import { observer } from 'mobx-react-lite';
-import React, { SyntheticEvent, useState } from 'react'
-import { Link } from 'react-router-dom';
-import { Button, Item, Label, Segment } from 'semantic-ui-react'
+import { Item, Segment } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store';
+import ActivityListItem from './ActivityListItem';
 
+export default observer(function ActivityList() {
+    const { activityStore } = useStore();
+    const { activitiesByDate, } = activityStore
 
-export default observer(function ActivityList(){
-    const {activityStore} = useStore();
-    const {deleteActivity, activitiesByDate,loading} = activityStore
-    const [target, setTarget] = useState('');
-    function handleActivityDelete(e:SyntheticEvent<HTMLButtonElement>, id: string){
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-        }
-    
-
-    return(
+    return (
         <Segment>
             <Item.Group divided>
-                {activitiesByDate.map(dashboard =>(
-                    <Item key={dashboard.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{dashboard.title}</Item.Header>
-                            <Item.Meta>{dashboard.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{dashboard.description}</div>
-                                <div>{dashboard.category}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button as={Link} to={`/activities/${dashboard.id}`} floated='right' content='view' color='blue'></Button>
-                                <Button 
-                                    name={dashboard.id}
-                                    loading={loading && target === dashboard.id} 
-                                    onClick={(e) => handleActivityDelete(e, dashboard.id)} 
-                                    floated='right' 
-                                    content='Delete' 
-                                    color='red'></Button>
-                                <Label basic content = {dashboard.category} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
+                {activitiesByDate.map(dashboard => (
+                    <ActivityListItem key={dashboard.id} dashboard={dashboard} />
                 ))}
             </Item.Group>
         </Segment>
