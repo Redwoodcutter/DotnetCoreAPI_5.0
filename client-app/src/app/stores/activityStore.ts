a019918ed07e849c6dd5439e73492ed15e06144b
redwoodcutter <oguzkumcular@gmail.com>
2021-06-15 23:39:59 +0300

    Group the activities

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,51 +1,61 @@
-import { makeAutoObservable, runInAction} from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
 
-export default class ActivityStore{
+export default class ActivityStore {
     activityRegistry = new Map<string, Activity>();
     SelectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
     loadingInitial = true;
 
-    constructor(){
-       makeAutoObservable(this)
+    constructor() {
+        makeAutoObservable(this)
     }
 
-    get activitiesByDate(){
-        return Array.from(this.activityRegistry.values()).sort((a,b)=> Date.parse(a.date)-Date.parse(b.date));
+    get activitiesByDate() {
+        return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+    }
+
+    get groupedActivities() {
+        return Object.entries(
+            this.activitiesByDate.reduce((dashboard, activity) => {
+                const date = activity.date;
+                dashboard[date] = dashboard[date] ? [...dashboard[date], activity] : [activity];
+                return dashboard;
+            }, {} as { [key: string]: Activity[]})
+        )
     }
 
     loadActivities = async () => {
         this.loadingInitial = true;
-        try{
+        try {
             const activities = await agent.Activities.list();
-            runInAction(()=>{
-                activities.forEach(activity =>{
-                   this.setActivity(activity);
-                  })
-                  this.loadingInitial = false; 
+            runInAction(() => {
+                activities.forEach(activity => {
+                    this.setActivity(activity);
+                })
+                this.loadingInitial = false;
             })
-        }catch(error){
+        } catch (error) {
             console.log(error)
-            runInAction(()=>{
-                this.loadingInitial = false; 
+            runInAction(() => {
+                this.loadingInitial = false;
             })
         }
     }
-    
-    loadActivity = async (id : string ) => {
+
+    loadActivity = async (id: string) => {
         let activity = this.getActivity(id);
-        if(activity){
+        if (activity) {
             this.SelectedActivity = activity;
             return activity;
-        }else{
+        } else {
             this.loadingInitial = true;
             try {
                 activity = await agent.Activities.details(id);
                 this.setActivity(activity);
-                runInAction(()=>{
+                runInAction(() => {
                     this.SelectedActivity = activity;
                 })
                 this.setLoadingInitial(false);
@@ -59,62 +69,62 @@ export default class ActivityStore{
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
     }
-    private getActivity = (id : string ) => {
+    private getActivity = (id: string) => {
         return this.activityRegistry.get(id);
     }
-    private setActivity = (activity : Activity) => {
+    private setActivity = (activity: Activity) => {
         activity.date = activity.date.split('T')[0];
         this.activityRegistry.set(activity.id, activity);
     }
 
 
-    createActivity  = async (activity : Activity) => {
-       this.loading = true;
-        try{
+    createActivity = async (activity: Activity) => {
+        this.loading = true;
+        try {
             await agent.Activities.create(activity);
-            runInAction(()=>{
+            runInAction(() => {
                 this.activityRegistry.set(activity.id, activity);
                 this.SelectedActivity = activity;
                 this.editMode = false;
                 this.loading = false;
             })
-        }catch(error){
+        } catch (error) {
             console.log(error)
-            runInAction(()=>{
+            runInAction(() => {
                 this.loading = false;
             })
         }
     }
 
-    updateActivity = async(activity: Activity)=>{
-        this.loading=true;
-        try{
+    updateActivity = async (activity: Activity) => {
+        this.loading = true;
+        try {
             await agent.Activities.update(activity);
-            runInAction(()=>{
+            runInAction(() => {
                 this.activityRegistry.set(activity.id, activity);
                 this.SelectedActivity = activity;
                 this.editMode = false;
                 this.loading = false;
             })
-        }catch(error){
+        } catch (error) {
             console.log(error)
-            runInAction(()=>{
+            runInAction(() => {
                 this.loading = false;
             })
         }
     }
-    deleteActivity = async (id: string ) => {
-        this.loading= true;
+    deleteActivity = async (id: string) => {
+        this.loading = true;
         try {
             await agent.Activities.delete(id);
-            runInAction(()=>{
+            runInAction(() => {
                 this.activityRegistry.delete(id);
                 this.loading = false;
             })
         } catch (error) {
             console.log(error);
-            runInAction(()=>{
-                this.loading=false;
+            runInAction(() => {
+                this.loading = false;
             })
         }
     }
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,19 +1,30 @@
 import { observer } from 'mobx-react-lite';
-import { Item, Segment } from 'semantic-ui-react'
+import { Fragment } from 'react';
+import { Header, Item, Segment } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store';
 import ActivityListItem from './ActivityListItem';
 
 export default observer(function ActivityList() {
     const { activityStore } = useStore();
-    const { activitiesByDate, } = activityStore
+    const { groupedActivities, } = activityStore
 
     return (
-        <Segment>
-            <Item.Group divided>
-                {activitiesByDate.map(dashboard => (
-                    <ActivityListItem key={dashboard.id} dashboard={dashboard} />
-                ))}
-            </Item.Group>
-        </Segment>
+        <>
+            {groupedActivities.map(([group, dashboard]) => (
+                <Fragment key={group}>
+                    <Header sub color='teal'>
+                    {group}
+                        <Segment>
+                            <Item.Group divided>
+                                {dashboard.map(activity => (
+                                    <ActivityListItem key={activity.id} dashboard={activity} />
+                                ))}
+                            </Item.Group>
+                        </Segment>
+                    </Header>
+                </Fragment>
+            ))}
+        </>
+
     )
 })
